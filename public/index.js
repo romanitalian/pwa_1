b84273cb19be57a1940bc38115a57fef19e199c7
romanitalian <romanitalian.net@gmail.com>
2020-04-21 22:27:56 +0300

    fix index.js - alive Add button

--- public/index.js
+++ public/index.js
@@ -13,50 +13,50 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-var isTooSoon = true;
-window.addEventListener("beforeinstallprompt", function(e) {
-  if (isTooSoon) {
-    e.preventDefault(); // Prevents prompt display
-    // Prompt later instead:
-    setTimeout(function() {
-      isTooSoon = false;
-      e.prompt(); // Throws if called more than once or default not prevented
-    }, 10000);
-  }
+// var isTooSoon = true;
+// window.addEventListener("beforeinstallprompt", function(e) {
+//   if (isTooSoon) {
+//     e.preventDefault(); // Prevents prompt display
+//     // Prompt later instead:
+//     setTimeout(function() {
+//       isTooSoon = false;
+//       e.prompt(); // Throws if called more than once or default not prevented
+//     }, 10000);
+//   }
 
-  // The event was re-dispatched in response to our request
-  // ...
-});
+//   // The event was re-dispatched in response to our request
+//   // ...
+// });
 
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
 
-// window.addEventListener('beforeinstallprompt', (e) => {
-//   console.log("111");
-//   // Prevent Chrome 67 and earlier from automatically showing the prompt
-//   e.preventDefault();
-//   // Stash the event so it can be triggered later.
-//   deferredPrompt = e;
-//   // Update UI to notify the user they can add to home screen
-//   addBtn.style.display = 'block';
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log("111");
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
 
-//   addBtn.addEventListener('click', (e) => {
-//     console.log("222");
-//     // hide our user interface that shows our A2HS button
-//     addBtn.style.display = 'none';
-//     // Show the prompt
-//     deferredPrompt.prompt();
-//     // Wait for the user to respond to the prompt
-//     deferredPrompt.userChoice.then((choiceResult) => {
-//         if (choiceResult.outcome === 'accepted') {
-//           console.log('User accepted the A2HS prompt');
-//         } else {
-//           console.log('User dismissed the A2HS prompt');
-//         }
-//         deferredPrompt = null;
-//       });
-//   });
-//   console.log("333");
-// });
+  addBtn.addEventListener('click', (e) => {
+    console.log("222");
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+  console.log("333");
+});
