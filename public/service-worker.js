09fd1d913bc9990ac8eb18f2deb152f4c548bd4b
romanitalian <romanitalian.net@gmail.com>
2020-04-21 21:28:32 +0300

    revived PWA; add posible to Add to Home screen on press button

---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    console.log('installing sw');
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
